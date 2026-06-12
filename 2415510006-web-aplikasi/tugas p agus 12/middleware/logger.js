94d60f95b8f78c47e46269422b598fa8b8739bca
kresnarisqiramadhan <kresnarizqiramadhan@gmail.com>
2026-06-12 15:18:24 +0700

    .

---
+++ 2415510006-web-aplikasi/tugas p agus 12/middleware/logger.js
@@ -0,0 +1,7 @@
+functionlogger (requestAnimationFrame, res next) {
+    const now = new Date () .toISOString();
+    console.log(`[${now}] ${req.method} ${req.originalUrl}`);
+    next ();
+}
+
+module.exports = logger;
